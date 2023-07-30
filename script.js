4cf56fc7c723766e0a0029263dd181a788ff4bca
João Campos <joaon.c.jv@gmail.com>
2023-07-30 14:41:56 -0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+let tamanho = 12
+
 function geradorDeSenha(){
+    let senha = []
     const caracteres = 'abcdefghjkmnpqrstuvwxyzABCDEFGHLKMNPQRSTUVWXYZ123456789@&!*'
 
-    let senha = []
-
-    for(let i = 0; i < 8; i++){
+    for(let i = 0; i < tamanho; i++){
         numeroAleatorio = Math.floor(Math.random() * caracteres.length)
         sorteio = caracteres[numeroAleatorio - 1]
-        senha.push(sorteio)
+        senha.push(sorteio)   
     }
-    return senha.join(',')
+    senha = senha.join('')
+    const inputEl = document.querySelector('#senha')
+    inputEl.value = senha
 }
-console.log(geradorDeSenha())
+
+const tamanhoEl = document.querySelector('#tamanho')
+tamanhoEl.addEventListener('input', function() {
+    tamanho = tamanhoEl.value
+    geradorDeSenha()
+
+})
+
+geradorDeSenha()
+
+
